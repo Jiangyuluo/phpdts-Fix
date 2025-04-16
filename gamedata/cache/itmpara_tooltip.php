84f58138744e066a230a8fcf27ccd60422456feb
Nemo Ma <nemoma@nmonolith.com>
2025-04-15 22:37:41 -0400

    maint

--- gamedata/cache/itmpara_tooltip.php
+++ gamedata/cache/itmpara_tooltip.php
@@ -12,7 +12,7 @@ if(!defined('IN_GAME')) exit('Access Denied');
  * - 'title'：显示的标题，如果为空则不显示标题
  * - 'format'：显示的格式，使用 {value} 作为占位符，会被实际值替换
  * - 'suffix'：值的后缀，如 '%'、'点' 等
- * - 'color'：显示的颜色，可选，默认为空
+ * - 'color'：显示的颜色，可选，默认为空 - 注意：由于目前对tooltip的设计，span元素实际上无法在tooltip中进行解析，因此设置了这个元素也只会显示解析前的span元素，除非完全重写逻辑，否则该元素可以认为是废弃的。
  * - 'condition'：条件函数，接收 $item_type 和 $value 两个参数，返回 true 表示显示，false 表示不显示
  *
  * 示例：
@@ -33,44 +33,36 @@ $itmpara_tooltip = [
         'title' => '最终伤害增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'red',
+        //'color' => 'red',
         'condition' => function($item_type, $value) {
-            // 如果是测试物品，则始终显示
-            if(strpos($item_type, '测试') !== false) {
-                return true;
-            }
-            return $item_type == 'W' || $item_type == 'D';
+            return true;
         }
     ],
     'AddDamagePercentage' => [
         'title' => '最终伤害增加',
         'format' => '{value}',
         'suffix' => '%',
-        'color' => 'red',
+        //'color' => 'red',
         'condition' => function($item_type, $value) {
-            // 如果是测试物品，则始终显示
-            if(strpos($item_type, '测试') !== false) {
-                return true;
-            }
-            return $item_type == 'W' || $item_type == 'D';
+			return true;
         }
     ],
     'DecreaseDamageRaw' => [
         'title' => '最终伤害减少',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'blue',
+        //'color' => 'blue',
         'condition' => function($item_type, $value) {
-            return $item_type == 'A' || $item_type == 'D';
+            return true;
         }
     ],
     'DecreaseDamagePercentage' => [
         'title' => '最终伤害减少',
         'format' => '{value}',
         'suffix' => '%',
-        'color' => 'blue',
+        //'color' => 'blue',
         'condition' => function($item_type, $value) {
-            return $item_type == 'A' || $item_type == 'D';
+            return true;
         }
     ],
 
@@ -79,7 +71,7 @@ $itmpara_tooltip = [
         'title' => '战斗中生命上限增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -88,7 +80,7 @@ $itmpara_tooltip = [
         'title' => '战斗中体力上限增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -97,7 +89,7 @@ $itmpara_tooltip = [
         'title' => '战斗中灵力上限增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -106,7 +98,7 @@ $itmpara_tooltip = [
         'title' => '战斗中攻击力增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -115,7 +107,7 @@ $itmpara_tooltip = [
         'title' => '战斗中防御力增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -124,7 +116,7 @@ $itmpara_tooltip = [
         'title' => '战斗中殴系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -133,7 +125,7 @@ $itmpara_tooltip = [
         'title' => '战斗中斩系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -142,7 +134,7 @@ $itmpara_tooltip = [
         'title' => '战斗中射系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -151,7 +143,7 @@ $itmpara_tooltip = [
         'title' => '战斗中投系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -160,7 +152,7 @@ $itmpara_tooltip = [
         'title' => '战斗中爆系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -169,7 +161,7 @@ $itmpara_tooltip = [
         'title' => '战斗中灵系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -178,7 +170,7 @@ $itmpara_tooltip = [
         'title' => '战斗中金钱增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -187,7 +179,7 @@ $itmpara_tooltip = [
         'title' => '战斗中怒气增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'yellow',
+        //'color' => 'yellow',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -198,7 +190,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中生命上限增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -207,16 +199,16 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中体力上限增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+       // 'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
     ],
     'AddPlayerMssInSearchMove' => [
-        'title' => '搜索/移动中灵力上限增加',
+        'title' => '搜索/移动中歌魂上限增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -225,7 +217,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中攻击力增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -234,7 +226,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中防御力增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -243,7 +235,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中殴系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -252,7 +244,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中斩系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -261,7 +253,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中射系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -270,7 +262,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中投系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+        //'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -279,7 +271,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中爆系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+       // 'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -288,7 +280,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中灵系熟练度增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+       // 'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -297,7 +289,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中金钱增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+       // 'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -306,7 +298,7 @@ $itmpara_tooltip = [
         'title' => '搜索/移动中怒气增加',
         'format' => '{value}',
         'suffix' => '',
-        'color' => 'green',
+      //  'color' => 'green',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -317,7 +309,7 @@ $itmpara_tooltip = [
         'title' => '平台物品',
         'format' => '此物品可以变身为特定角色',
         'suffix' => '',
-        'color' => 'purple',
+      //  'color' => 'purple',
         'condition' => function($item_type, $value) {
             return $value == 1;
         }
@@ -326,7 +318,7 @@ $itmpara_tooltip = [
         'title' => '限时变身',
         'format' => '变身效果会随着时间消失',
         'suffix' => '',
-        'color' => 'purple',
+        //'color' => 'purple',
         'condition' => function($item_type, $value) {
             return $value == 1;
         }
@@ -335,7 +327,7 @@ $itmpara_tooltip = [
         'title' => '变身持续时间',
         'format' => '{value}',
         'suffix' => '回合',
-        'color' => 'purple',
+        //'color' => 'purple',
         'condition' => function($item_type, $value) {
             return true;
         }
@@ -346,7 +338,7 @@ $itmpara_tooltip = [
         'title' => '任务物品',
         'format' => '此物品与任务相关',
         'suffix' => '',
-        'color' => 'orange',
+        //'color' => 'orange',
         'condition' => function($item_type, $value) {
             return $value == 1;
         }
